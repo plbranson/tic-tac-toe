8786388af388f1a364820a97cad40bcb6a076b4a
plbranson <patrick.l.branson@plbranson>
2023-03-09 22:30:29 -0500

    Finished Product

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -13,3 +13,81 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
+const gameBoard = document.querySelector('#game-board')
+const information = document.querySelector('#information')
+
+const startCells = new Array(9).fill('')
+information.textContent = ''
+
+let startItem = 'circle'
+information.textContent = 'Circle Goes First'
+
+function initializeGameBoard() {
+  // The underscore in the "forEach" function means that parameter
+  // is not being used. I did not know that until today (March 9, 2023 21:44 EST)
+  startCells.forEach((_cell, index) => {
+    const cellElement = document.createElement('div')
+    cellElement.classList.add('square')
+    cellElement.id = index
+    cellElement.addEventListener('click', addItem)
+    gameBoard.append(cellElement)
+  })
+}
+
+function addItem(event) {
+  const display = document.createElement('div')
+  display.classList.add(startItem)
+  event.target.append(display)
+
+  // Changes the starting item for each game cycle
+  startItem = startItem === 'circle' ? 'cross' : 'circle'
+  information.textContent = 'It is now ' + startItem + "'s turn"
+
+  event.target.removeEventListener('click', addItem)
+  checkScore()
+}
+
+function checkScore() {
+  const allSquares = document.querySelectorAll('.square')
+
+  // An Array of the all possible winning combination
+  const winningCombination = [
+    /* Horizontal Winning Combinations */
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+
+    /* Vertical Winning Combinations */
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+    /* Diagonal Winning Combinations */
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  winningCombination.forEach((array) => {
+    const circleWins = array.every((cell) => allSquares[cell].firstChild?.classList.contains('circle'))
+
+    if (circleWins) {
+      information.textContent = 'Circle Wins!'
+      allSquares.forEach((square) => square.replaceWith(square.cloneNode(true)))
+      return
+    }
+  })
+
+  winningCombination.forEach((array) => {
+    const crossWins = array.every((cell) => allSquares[cell].firstChild?.classList.contains('cross'))
+
+    if (crossWins) {
+      information.textContent = 'Cross Wins!'
+      allSquares.forEach((square) => square.replaceWith(square.cloneNode(true)))
+      return
+    }
+  })
+}
+
+// Initializes and runs the game
+initializeGameBoard()
